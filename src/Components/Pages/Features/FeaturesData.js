3a880ee8b683ce2bc1d4e11e27148cd0478bf01a
teelda <matildaipeh@gmail.com>
2021-12-11 07:45:49 +0100

    Create FeaturesData.js
    The features component data

---
+++ src/Components/Pages/Features/FeaturesData.js
@@ -0,0 +1,27 @@
+export const featuresObjOne = {
+    headings: 'Admin & Logistics',
+    subDesc:
+      'Manage and track company assets as well as logistics for travelling employees',
+    iconImg: 'Images/travel-svg.svg',
+    subHeading: 'Travel & Logistics',
+    smallDesc:'Make travel requests, get approvals, and have access to travel information.',
+    iconImg1: 'Images/asset-svg.svg',
+    subHeading1: 'Asset Management',
+    smallDesc1:'Manage the acquisition, assignment, and disposition of assets seamlessly. travel requests, get approvals, and have access to travel information.',
+    alt: 'Resource Edge',
+};
+
+export const featuresObjTwo = {
+    headings: 'Finance',
+    subDesc:
+      'Generate invoices, track expenditure, and manage complex payrolls for multiple teams and companies',
+    iconImg: 'Images/vouchers-svg.svg',
+    subHeading: 'RE Vouchers',
+    smallDesc:'Track and manage expenditure for multiple teams across your organisation using Resource Edge Vouchers',
+    iconImg1: 'Images/payroll-svg.svg',
+    subHeading1: 'Payroll',
+    smallDesc1:'Our easy to use systems takes away the pain of managing complex payrolls for organisations of all sizes.',
+    alt: 'Resource Edge',
+};
+
+
