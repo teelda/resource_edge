12daf622432ced84943dcbe719d9d40d6bfb1182
teelda <matildaipeh@gmail.com>
2021-12-11 07:50:37 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Pages/HomePage/Home';
+import About from './Components/Pages/About/About';
+import Features from './Components/Pages/Features/Features';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <About />
+      <Features />
+    </Router>
+  )
+}
+
+export default App
