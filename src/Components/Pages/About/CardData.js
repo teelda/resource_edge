66768c0698b87f86f90d6d790bfee5b01c234fa0
teelda <matildaipeh@gmail.com>
2021-12-11 07:45:06 +0100

    Create CardData.js
    The card component

---
+++ src/Components/Pages/About/CardData.js
@@ -0,0 +1,24 @@
+export const aboutObjOne = {
+    icon: 'Images/card1-svg.svg',
+    alt:'',
+    topLine:'Employee Management',
+    subDescription:'From Hiring & Onboarding to Retiring, the Resource Edge Employee Management module eliminates all the complexities & paperwork involved in managing your team.',
+};
+
+export const aboutObjTwo = {
+    icon: 'Images/card2-svg.svg',
+    alt:'',
+    topLine:'Performance Management',
+    subDescription:'Manage and track employee performance with our easy-to-use tools for goal setting, performance agreements and performance reviews. ',
+};
+
+export const aboutObjThree = {
+    icon: 'Images/card3-svg.svg',
+    alt:'',
+    topLine:'Paid time off',
+    subDescription:'Employees can request for paid time off, vacations, sick leaves or educational leaves and get approvals all within Resource Edge. HR managers can equally ensure compliance.',
+};
+
+// export const imageObj = {
+//     image: 'Images/about-svg.svg',
+// };
